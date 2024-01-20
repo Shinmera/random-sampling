22f5c66107d0ef301cebe8e52073f76a4462a7bc
Shinmera <shinmera@tymoon.eu>
2024-01-20 23:39:52 +0100

    bla

--- documentation.lisp
+++ documentation.lisp
@@ -1,1 +1,191 @@
 (in-package #:org.shirakumo.random-sampling)
+
+;; distributions.lisp
+(docs:define-docs
+  (function gaussian
+    "")
+  
+ (function gaussian-pdf
+   "")
+ 
+ (function gaussian-tail
+   "")
+ 
+ (function gaussian-tail-pdf
+   "")
+ 
+ (function exponential
+   "")
+ 
+ (function exponential-pdf
+   "")
+ 
+ (function laplace
+   "")
+ 
+ (function laplace-pdf
+   "")
+ 
+ (function cauchy
+   "")
+ 
+ (function cauchy-pdf
+   "")
+ 
+ (function rayleigh
+   "")
+ 
+ (function rayleigh-pdf
+   "")
+ 
+ (function rayleigh-tail
+   "")
+ 
+ (function rayleigh-tail-pdf
+   "")
+ 
+ (function levy
+   "")
+ 
+ (function levy-skew
+   "")
+ 
+ (function gamma
+   "")
+ 
+ (function gamma-pdf
+   "")
+ 
+ (function flat
+   "")
+ 
+ (function flat-pdf
+   "")
+ 
+ (function lognormal
+   "")
+ 
+ (function lognormal-pdf
+   "")
+ 
+ (function chi-squared
+   "")
+ 
+ (function chi-squared-pdf
+   "")
+ 
+ (function fdist
+   "")
+ 
+ (function fdist-pdf
+   "")
+ 
+ (function tdist
+   "")
+ 
+ (function tdist-pdf
+   "")
+ 
+ (function beta
+   "")
+ 
+ (function beta-pdf
+   "")
+ 
+ (function logistic
+   "")
+ 
+ (function logistic-pdf
+   "")
+ 
+ (function pareto
+   "")
+ 
+ (function pareto-pdf
+   "")
+ 
+ (function weibull
+   "")
+ 
+ (function weibull-pdf
+   "")
+ 
+ (function gumbel-1
+   "")
+ 
+ (function gumbel-1-pdf
+   "")
+ 
+ (function gumbel-2
+   "")
+ 
+ (function gumbel-2-pdf
+   "")
+ 
+ (function poisson
+   "")
+ 
+ (function poisson-pdf
+   "")
+ 
+ (function bernoulli
+   "")
+ 
+ (function bernoulli-pdf
+   "")
+ 
+ (function binomial
+   "")
+ 
+ (function binomial-pdf
+   "")
+ 
+ (function -binomial
+   "")
+ 
+ (function -binomial-pdf
+   "")
+ 
+ (function gemoetric
+   "")
+ 
+ (function geometric-pdf
+   "")
+ 
+ (function logarithmic
+   "")
+ 
+ (function logarithmic-pdf
+   ""))
+
+;; volumes.lisp
+(docs:define-docs
+  (function rejection-sample
+    "")
+  
+  (function sphere
+    "")
+  
+  (function half-sphere
+    "")
+  
+  (function normal
+    "")
+  
+  (function disc
+    "")
+  
+  (function box
+    "")
+  
+  (function cylinder
+    "")
+  
+  (function pill
+    "")
+  
+  (function triangle
+    "")
+  
+  (function convex-mesh
+    ""))
